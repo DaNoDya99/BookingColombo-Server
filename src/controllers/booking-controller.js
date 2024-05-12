f88b921a00a248acc16a0eee29cf336420df51eb
Gagana Samarasekara <gaganasamarasekara8@gmail.com>
2024-05-12 20:23:18 +0530

    booking functions added

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -28,6 +28,34 @@ class BookingController{
             res.status(500).json(e.message);
         }
     }
+
+    async deleteBooking(req, res) {
+        try {
+            const id = req.params.id;
+            const deletedBooking = await BookingService.deleteBooking(id);
+            if(deletedBooking) {
+                return res.status(200).json({status: "success", message: "Booking deleted successfully"});
+            } else {
+                return res.status(500).json({status: "error", message: "An error occurred"});
+            }
+        } catch (error) {
+            return res.status(500).json({error: error.message});
+        }
+    }
+
+    async getBookingsByID(req, res){
+        try{
+            const id = req.params.id;
+            const bookings = await BookingService.getBookingsByID(id);
+            if(bookings){
+                res.status(200).json({ status: "success", message: "All Bookings fetched successfully", bookings: bookings});
+            }else{
+                res.status(500).json({ status: "success", message: "An error occurred", bookings: []})
+            }
+        }catch (e){
+            res.status(500).json(e.message);
+        }
+    }
 }
 
 module.exports = new BookingController();
--- src/routes/booking-routes.js
+++ src/routes/booking-routes.js
@@ -4,5 +4,7 @@ const BookingController = require('../controllers/booking-controller');
 
 router.route('/add-booking').post(BookingController.addBooking);
 router.route('/get-bookings/:arrival_date').get(BookingController.getBookingsByArrivalDate);
+router.route('/delete-bookings/:id').delete(BookingController.deleteBooking);
+router.route('/get-all-bookings/:id').get(BookingController.getBookingsByID);
 
 module.exports = router;
